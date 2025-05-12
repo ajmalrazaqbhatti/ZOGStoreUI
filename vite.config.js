6ab4699b83d5caf13a97f7d28bfc089abd9f6b33
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2025-05-13 01:37:56 +0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Get the repository name from package.json or environment variables
-const repoName = 'ZOGStoreUI';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.MODE === 'production' ? `/${repoName}/` : '/',
 });
